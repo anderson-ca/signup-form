4d5c354632b33c6afcd317fc5fbe125cabb6e7e9
anderson <anderson.d.cardoso92@gmail.com>
2022-06-16 10:17:17 -0500

    finish form input validation

--- index.js
+++ index.js
@@ -9,11 +9,17 @@ let regex = RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  console.log(regex.test(email.children[0].value));
+
   if (first_name.children[0].value === "") {
     first_name.children[0].placeholder = "";
     first_name.children[0].classList.add("error");
     first_name.children[1].classList.remove("noshow");
     first_name.children[2].classList.remove("noshow");
+  } else {
+    first_name.children[0].classList.remove("error");
+    first_name.children[1].classList.add("noshow");
+    first_name.children[2].classList.add("noshow");
   }
 
   if (last_name.children[0].value === "") {
@@ -21,6 +27,10 @@ form.addEventListener("submit", (e) => {
     last_name.children[0].classList.add("error");
     last_name.children[1].classList.remove("noshow");
     last_name.children[2].classList.remove("noshow");
+  } else {
+    last_name.children[0].classList.remove("error");
+    last_name.children[1].classList.add("noshow");
+    last_name.children[2].classList.add("noshow");
   }
 
   if (email.children[0].value === "") {
@@ -28,6 +38,17 @@ form.addEventListener("submit", (e) => {
     email.children[0].classList.add("error");
     email.children[1].classList.remove("noshow");
     email.children[2].classList.remove("noshow");
+    email.children[3].classList.add("noshow");
+  } else if (!regex.test(email.children[0].value)) {
+    email.children[0].classList.add("error");
+    email.children[1].classList.remove("noshow");
+    email.children[2].classList.add("noshow");
+    email.children[3].classList.remove("noshow");
+  } else {
+    email.children[0].classList.remove("error");
+    email.children[1].classList.add("noshow");
+    email.children[2].classList.add("noshow");
+    email.children[3].classList.add("noshow");
   }
 
   if (password.children[0].value === "") {
@@ -35,5 +56,9 @@ form.addEventListener("submit", (e) => {
     password.children[0].classList.add("error");
     password.children[1].classList.remove("noshow");
     password.children[2].classList.remove("noshow");
+  } else {
+    password.children[0].classList.remove("error");
+    password.children[1].classList.add("noshow");
+    password.children[2].classList.add("noshow");
   }
 });
