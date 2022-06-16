4472ce0b0c74937b26dc122a9ed3b8f759cc55b7
anderson <anderson.d.cardoso92@gmail.com>
2022-06-16 09:44:43 -0500

    add basic form input error handling

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-let firstName = document.getElementById("first_name");
-let lastName = document.getElementById("last_name");
-let email = document.getElementById("email");
+let first_name = document.getElementById("firstName");
+let last_name = document.getElementById("lastName");
+let email = document.getElementById("Email");
+let password = document.getElementById("Password");
 let form = document.getElementById("form");
+let errorElements = document.getElementsByClassName("noshow");
 let regex = RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  if (firstName.value === "") {
-    firstName.classList.add("error");
-    firstName.innerHTML = "<i class='material-icons'>error</i>";
+  if (first_name.children[0].value === "") {
+    first_name.children[0].placeholder = "";
+    first_name.children[0].classList.add("error");
+    first_name.children[1].classList.remove("noshow");
+    first_name.children[2].classList.remove("noshow");
   }
 
-  if (lastName.value === "") {
-    lastName.classList.add("error");
+  if (last_name.children[0].value === "") {
+    last_name.children[0].placeholder = "";
+    last_name.children[0].classList.add("error");
+    last_name.children[1].classList.remove("noshow");
+    last_name.children[2].classList.remove("noshow");
   }
 
-  if (email.value === "") {
-    email.classList.add("error");
+  if (email.children[0].value === "") {
+    email.children[0].placeholder = "";
+    email.children[0].classList.add("error");
+    email.children[1].classList.remove("noshow");
+    email.children[2].classList.remove("noshow");
   }
 
-  if (password.value === "") {
-    password.classList.add("error");
+  if (password.children[0].value === "") {
+    password.children[0].placeholder = "";
+    password.children[0].classList.add("error");
+    password.children[1].classList.remove("noshow");
+    password.children[2].classList.remove("noshow");
   }
 });
